9a97981d1954e477097a66caf7773a4220dcaabb
Harriet Craven <hc6@sanger.ac.uk>
2018-04-26 12:07:42 +0100

    Add local dev config for work order url

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  WORK_ORDER_URL: '"https://dev.psd.sanger.ac.uk:9200/work-orders"',
+  WORK_ORDER_URL: '"http://localhost:3200"',
 })
