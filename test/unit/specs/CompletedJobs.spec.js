e4634c44d4f125a87d860aaad328a323b0c14809
Harriet Craven <hc6@sanger.ac.uk>
2018-05-02 10:02:50 +0100

    Added basic tests for job table components

---
+++ test/unit/specs/CompletedJobs.spec.js
@@ -0,0 +1,40 @@
+import { mount } from '@vue/test-utils'
+import Vue from 'vue';
+import CompletedJobs from '@/components/CompletedJobs';
+
+describe('CompletedJobs.vue', () => {
+
+  it('has a name', () => {
+    expect(CompletedJobs.name).to.equal('completed-jobs');
+  });
+
+  it('has data', () => {
+    expect(CompletedJobs.data).to.be.a('function');
+  });
+
+  it('has a title', () => {
+    const wrapper = mount(CompletedJobs, { data: { totalCompletedJobs: 12 }});
+    expect(wrapper.html()).to.contain('Completed jobs (12)');
+  });
+
+  it('has a table', () => {
+    const wrapper = mount(CompletedJobs);
+    expect(wrapper.contains('b-table')).to.eq(true);
+  });
+
+  it('has set data defined', () => {
+    const wrapper = mount(CompletedJobs);
+
+    expect(wrapper.vm.fields.length).to.eq(12);
+    expect(wrapper.vm.isBusy).to.equal(false);
+    expect(wrapper.vm.currentPage).to.equal(1);
+    expect(wrapper.vm.perPage).to.equal(5);
+    expect(wrapper.vm.pageOptions).to.be.a('array');
+    expect(wrapper.vm.totalCompletedJobs).to.equal(0);
+    expect(wrapper.vm.sortBy).to.equal('date-requested');
+    expect(wrapper.vm.sortDesc).to.equal(false);
+    expect(wrapper.vm.items).to.be.a('array');
+    expect(wrapper.vm.detailedItems).to.be.a('object');
+  })
+
+});
--- test/unit/specs/Inbox.spec.js
+++ test/unit/specs/Inbox.spec.js
@@ -1,5 +1,6 @@
 require('es6-promise').polyfill();
 
+import { mount } from '@vue/test-utils'
 import Vue from 'vue';
 import Inbox from '@/components/Inbox';
 
@@ -14,14 +15,11 @@ describe('Inbox.vue', () => {
   })
 
   it('has imported components', () => {
-    const Constructor = Vue.extend(Inbox)
-    const vm = new Constructor({}).$mount();
-    expect(vm.$el.textContent).to.include('Aker');
-    expect(vm.$el.textContent).to.include('Queued jobs');
-    expect(vm.$el.textContent).to.include('Jobs in progress');
-    expect(vm.$el.textContent).to.include('Completed jobs');
-
-    expect(vm.$options.beforeCreate).to.be.defined;
+    const wrapper = mount(Inbox);
+    expect(wrapper.html()).to.contain('Aker');
+    expect(wrapper.html()).to.contain('Queued jobs');
+    expect(wrapper.html()).to.contain('Jobs in progress');
+    expect(wrapper.html()).to.contain('Completed jobs');
   })
 
 });
